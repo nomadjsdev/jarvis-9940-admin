2613c33525ce611f3e41751e6be72b73a4bb5329
James <58656026+nomadjsdev@users.noreply.github.com>
2020-01-29 22:05:40 +0700

    Initial button styling. Delete items.

--- src/Component/Session/Row.js
+++ src/Component/Session/Row.js
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 
 const Row = styled.div`
 	width: 100%;
-	padding: 20px 0;
+	padding: 20px;
 	border: 1px solid black;
 	box-shadow: 2px 2px;
 `
--- src/View/Edit/EditTemplate.js
+++ src/View/Edit/EditTemplate.js
@@ -19,13 +19,17 @@ const EditTemplate = props => {
 	const [values, setValues] = useState({})
 
 	const [isEditing, setIsEditing] = useState(false)
-	const [isEditingText, setIsEditingText] = useState(false)
+	const [isEditingButton, setIsEditingButton] = useState(false)
 	const [editButton, setEditButton] = useState(null)
 
 	const resetTemplate = () => {
 		return encounterTemplates[selectedEncounter] ?? []
 	}
 	const [template, setTemplate] = useState(resetTemplate())
+
+	// This is a quick-and-dirty deep clone of the template nested array
+	// FIXME: There's got to be a better way of doing this!
+	const deepCopyArray = array => JSON.parse(JSON.stringify(array))
 
 	const saveTemplate = () => {
 		// TODO: Prevent writing empty arrays
@@ -51,6 +55,17 @@ const EditTemplate = props => {
 				>
 					{isEditing ? 'Cancel' : 'Edit template'}
 				</Button>
+				{isEditing && (
+					<Button
+						type="button"
+						variant="add"
+						onClick={() => {
+							saveTemplate()
+						}}
+					>
+						Save
+					</Button>
+				)}
 			</div>
 			{isAdding && (
 				<ModalContainer>
@@ -61,9 +76,7 @@ const EditTemplate = props => {
 								type="button"
 								variant="add"
 								onClick={() => {
-									// This is a quick-and-dirty deep clone of the template nested array
-									// FIXME: There's got to be a better way of doing this!
-									let newTemplate = JSON.parse(JSON.stringify(template))
+									let newTemplate = deepCopyArray(template)
 									newTemplate[addRow][addCol].push({
 										id: safeNanoid(),
 										text: '',
@@ -138,9 +151,7 @@ const EditTemplate = props => {
 									for (let i = 0; i < number; i++) {
 										buttonArray.push({ id: safeNanoid(), text: '', type: 'toggle' })
 									}
-									// This is a quick-and-dirty deep clone of the template nested array
-									// FIXME: There's got to be a better way of doing this!
-									let newTemplate = JSON.parse(JSON.stringify(template))
+									let newTemplate = deepCopyArray(template)
 									newTemplate[addRow][addCol].push({
 										id: safeNanoid(),
 										direction: values.direction,
@@ -172,17 +183,17 @@ const EditTemplate = props => {
 					</ModalContents>
 				</ModalContainer>
 			)}
-			{isEditingText && (
+			{isEditingButton && (
 				<ModalContainer>
 					<ModalContents>
 						<h2>Edit button text</h2>
-						{values.buttonText && (
+						{(values.buttonText || values?.buttonText === '') && (
 							<>
 								<p>
 									Update button label:
 									<input
 										type="text"
-										value={values.buttonText}
+										value={values.buttonText || ''}
 										onChange={e => {
 											setValues({ buttonText: e.target.value })
 										}}
@@ -192,15 +203,13 @@ const EditTemplate = props => {
 									type="button"
 									variant="add"
 									onClick={() => {
-										// This is a quick-and-dirty deep clone of the template nested array
-										// FIXME: There's got to be a better way of doing this!
-										let newTemplate = JSON.parse(JSON.stringify(template))
+										let newTemplate = deepCopyArray(template)
 										newTemplate[addRow][addCol][editButton].text = values.buttonText
 										setTemplate(newTemplate)
 										setAddRow(null)
 										setAddCol(null)
 										setEditButton(null)
-										setIsEditingText(false)
+										setIsEditingButton(false)
 										setValues({})
 									}}
 								>
@@ -225,19 +234,49 @@ const EditTemplate = props => {
 										/>
 									</p>
 								))}
+								<p>
+									Update direction:
+									<label htmlFor="editVert">Vertical</label>
+									<input
+										type="radio"
+										name="direction"
+										id="editVert"
+										value="vertical"
+										checked={values.direction === 'vertical'}
+										onChange={e => {
+											const { value } = e.target
+											setValues(prevState => {
+												return { ...prevState, direction: value }
+											})
+										}}
+									/>
+									<label htmlFor="editHori">Horizontal</label>
+									<input
+										type="radio"
+										name="direction"
+										id="editHori"
+										value="horizontal"
+										checked={values.direction === 'horizontal'}
+										onChange={e => {
+											const { value } = e.target
+											setValues(prevState => {
+												return { ...prevState, direction: value }
+											})
+										}}
+									/>
+								</p>
 								<Button
 									type="button"
 									variant="add"
 									onClick={() => {
-										// This is a quick-and-dirty deep clone of the template nested array
-										// FIXME: There's got to be a better way of doing this!
-										let newTemplate = JSON.parse(JSON.stringify(template))
+										let newTemplate = deepCopyArray(template)
 										newTemplate[addRow][addCol][editButton].buttons = values.buttons
+										newTemplate[addRow][addCol][editButton].direction = values.direction
 										setTemplate(newTemplate)
 										setAddRow(null)
 										setAddCol(null)
 										setEditButton(null)
-										setIsEditingText(false)
+										setIsEditingButton(false)
 										setValues({})
 									}}
 								>
@@ -251,7 +290,7 @@ const EditTemplate = props => {
 								setAddRow(null)
 								setAddCol(null)
 								setEditButton(null)
-								setIsEditingText(false)
+								setIsEditingButton(false)
 								setValues({})
 							}}
 						>
@@ -261,10 +300,13 @@ const EditTemplate = props => {
 							variant="delete"
 							onClick={() => {
 								// TODO: Delete button from template
+								let newTemplate = deepCopyArray(template)
+								newTemplate[addRow][addCol].splice(editButton, 1)
+								setTemplate(newTemplate)
 								setAddRow(null)
 								setAddCol(null)
 								setEditButton(null)
-								setIsEditingText(false)
+								setIsEditingButton(false)
 								setValues({})
 							}}
 						>
@@ -280,7 +322,7 @@ const EditTemplate = props => {
 						<Row key={rowIndex} style={{ display: 'flex', flexFlow: 'row nowrap' }}>
 							{row.map((col, colIndex) => {
 								return (
-									<div key={colIndex}>
+									<div key={colIndex} style={{ flexBasis: `${(1 / row.length) * 100}%`, textAlign: 'center' }}>
 										{col.map((button, buttonIndex) => {
 											if (button.type === 'toggle') {
 												return (
@@ -305,141 +347,173 @@ const EditTemplate = props => {
 					{template &&
 						template.map((row, rowIndex) => {
 							return (
-								<Row key={rowIndex} style={{ display: 'flex', flexFlow: 'row nowrap' }}>
-									{row &&
-										row.map((col, colIndex) => {
-											return (
-												<div key={colIndex}>
-													<div
-														style={{
-															width: '100%',
-															display: 'flex',
-															flexDirection: 'column',
-															flexBasis: `100 / ${row.length}%`,
-															border: '1px solid red',
-														}}
-													>
-														{col &&
-															col.map((button, buttonIndex) => {
-																if (button.type === 'toggle') {
-																	return (
-																		<div key={buttonIndex}>
-																			<ToggleButton id={button.id}>{button.text}</ToggleButton>
-																			<button
-																				onClick={() => {
-																					setIsEditingText(true)
-																					setAddRow(rowIndex)
-																					setAddCol(colIndex)
-																					setEditButton(buttonIndex)
-																					setValues({ buttonText: button.text })
-																				}}
-																			>
-																				e
-																			</button>
-																		</div>
-																	)
-																}
-																if (button.type === 'group') {
-																	return (
-																		<div key={buttonIndex}>
-																			<ButtonGroup orientation={button.direction} buttons={button.buttons} />
-																			<button
-																				onClick={() => {
-																					setIsEditingText(true)
-																					setAddRow(rowIndex)
-																					setAddCol(colIndex)
-																					setEditButton(buttonIndex)
-																					setValues({ buttons: button.buttons })
-																				}}
-																			>
-																				e
-																			</button>
-																		</div>
-																	)
-																}
-
-																return false
-															})}
-
-														<Button
-															type="button"
-															variant="add"
-															onClick={() => {
-																setAddRow(rowIndex)
-																setAddCol(colIndex)
-																setIsAdding(true)
+								<div key={rowIndex} style={{ display: 'flex', flexFlow: 'row nowrap' }}>
+									<Row style={{ display: 'flex', flexFlow: 'row nowrap' }}>
+										{row &&
+											row.map((col, colIndex) => {
+												return (
+													<div key={colIndex} style={{ flexBasis: `${(1 / row.length) * 100}%`, textAlign: 'center' }}>
+														<div
+															style={{
+																width: '100%',
+																display: 'flex',
+																flexDirection: 'column',
+																flexBasis: `100 / ${row.length}%`,
+																border: '1px solid red',
 															}}
 														>
-															Add item
-														</Button>
+															{col &&
+																col.map((button, buttonIndex) => {
+																	if (button.type === 'toggle') {
+																		return (
+																			<div key={buttonIndex}>
+																				<ToggleButton id={button.id}>{button.text}</ToggleButton>
+																				<button
+																					onClick={() => {
+																						setIsEditingButton(true)
+																						setAddRow(rowIndex)
+																						setAddCol(colIndex)
+																						setEditButton(buttonIndex)
+																						setValues({ buttonText: button.text || '' })
+																					}}
+																				>
+																					e
+																				</button>
+																			</div>
+																		)
+																	}
+																	if (button.type === 'group') {
+																		return (
+																			<div key={buttonIndex}>
+																				<ButtonGroup orientation={button.direction} buttons={button.buttons} />
+																				<button
+																					onClick={() => {
+																						setIsEditingButton(true)
+																						setAddRow(rowIndex)
+																						setAddCol(colIndex)
+																						setEditButton(buttonIndex)
+																						setValues({ buttons: button.buttons, direction: button.direction })
+																					}}
+																				>
+																					e
+																				</button>
+																			</div>
+																		)
+																	}
+
+																	return false
+																})}
+
+															<button
+																type="button"
+																style={{
+																	width: '100%',
+																	margin: '20px 0',
+																	padding: '10px',
+																	backgroundColor: 'white',
+																	color: 'green',
+																	border: '1px solid green',
+																	borderRadius: '5px',
+																}}
+																onClick={() => {
+																	setAddRow(rowIndex)
+																	setAddCol(colIndex)
+																	setIsAdding(true)
+																}}
+															>
+																Add item
+															</button>
+														</div>
+														<button
+															type="button"
+															style={{
+																width: '100%',
+																margin: '20px 0',
+																padding: '10px',
+																backgroundColor: 'white',
+																color: 'red',
+																border: '1px solid red',
+																borderRadius: '5px',
+															}}
+															onClick={() => {
+																// FIXME: There's got to be a better way of doing this!
+																let newTemplate = deepCopyArray(template)
+																newTemplate[rowIndex].splice(colIndex, 1)
+																setTemplate(newTemplate)
+															}}
+														>
+															Delete column
+														</button>
 													</div>
-													<Button
-														type="button"
-														variant="delete"
-														onClick={() => {
-															// FIXME: There's got to be a better way of doing this!
-															let newTemplate = JSON.parse(JSON.stringify(template))
-															newTemplate[rowIndex].splice(colIndex, 1)
-															setTemplate(newTemplate)
-														}}
-													>
-														Delete column
-													</Button>
-												</div>
-											)
-										})}
-									<Button
+												)
+											})}
+										<div style={{ textAlign: 'center' }}>
+											<button
+												type="button"
+												style={{
+													height: '100%',
+													margin: '0 20px',
+													padding: '10px',
+													backgroundColor: 'white',
+													color: 'green',
+													border: '1px solid green',
+													borderRadius: '5px',
+												}}
+												onClick={() => {
+													// FIXME: There's got to be a better way of doing this!
+													let newTemplate = deepCopyArray(template)
+													newTemplate[rowIndex].push([])
+													setTemplate(newTemplate)
+												}}
+											>
+												Add column
+											</button>
+										</div>
+									</Row>
+									<button
 										type="button"
-										variant="add"
-										onClick={() => {
-											// FIXME: There's got to be a better way of doing this!
-											let newTemplate = JSON.parse(JSON.stringify(template))
-											newTemplate[rowIndex].push([])
-											setTemplate(newTemplate)
+										style={{
+											margin: '0 0 0 20px',
+											padding: '10px',
+											backgroundColor: 'white',
+											color: 'red',
+											border: '1px solid red',
+											borderRadius: '5px',
 										}}
-									>
-										Add column
-									</Button>
-									<Button
-										type="button"
-										variant="delete"
 										onClick={() => {
 											// FIXME: There's got to be a better way of doing this!
-											let newTemplate = JSON.parse(JSON.stringify(template))
+											let newTemplate = deepCopyArray(template)
 											newTemplate.splice(rowIndex, 1)
 											setTemplate(newTemplate)
 										}}
 									>
 										Delete row
-									</Button>
-								</Row>
+									</button>
+								</div>
 							)
 						})}
 					<>
 						<div>
-							<Button
+							<button
 								type="button"
-								variant="add"
+								style={{
+									width: '100%',
+									margin: '20px 0',
+									padding: '10px',
+									backgroundColor: 'white',
+									color: 'green',
+									border: '1px solid green',
+									borderRadius: '5px',
+								}}
 								onClick={() => {
 									// FIXME: There's got to be a better way of doing this!
-									let newTemplate = JSON.parse(JSON.stringify(template))
+									let newTemplate = deepCopyArray(template)
 									newTemplate.push([])
 									setTemplate(newTemplate)
 								}}
 							>
 								Add row
-							</Button>
-						</div>
-						<div>
-							<Button
-								type="button"
-								variant="add"
-								onClick={() => {
-									saveTemplate()
-								}}
-							>
-								Save
-							</Button>
+							</button>
 						</div>
 					</>
 				</>
